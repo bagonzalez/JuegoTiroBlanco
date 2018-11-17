bd4d0463018c2eeb6000ba77cf3c03ab81a34868
Docente <Docente@L202iMacDocente.local>
2018-11-17 15:28:19 -0600

    multiplayer init

---
+++ Assets/Scripts/DetectarColision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectarColision : MonoBehaviour {
+
+    public GameObject puntaje;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("inicio");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+        Debug.Log("Colision Objeto" + collision.gameObject.name);
+
+        if(collision.gameObject.tag == "bola"){
+			Puntaje puntajeScript = puntaje.GetComponent<Puntaje>();
+			puntajeScript.addPuntaje();
+            
+        }
+	}
+}
