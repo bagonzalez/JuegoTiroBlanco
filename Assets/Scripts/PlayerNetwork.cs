5c8ee9976ad1c1e5d5d634f0a659f1557ed66a52
Docente <Docente@L202iMacDocente.local>
2018-11-17 15:59:48 -0600

    multiplayer CONT

---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerNetwork : NetworkBehaviour {
+
+    public GameObject firstPersonCharacter;
+    public GameObject[] characterModel;
+
+    public override void OnStartLocalPlayer(){
+        //base.OnStartLocalPlayer();
+
+        GetComponent<FirstPersonController>().enabled = true;
+
+    }
+}
